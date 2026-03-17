83e9893b0a3770cfe24dc8b1df1375cbaddd0b3e
Lucas BT <l.blanchard-toulet@outlook.fr>
2026-03-17 16:59:59 +0100

    API generation de mot de passe aléatoire

--- src/Form/EmployeType.php
+++ src/Form/EmployeType.php
@@ -6,7 +6,8 @@ use App\Entity\Employe;
 use App\Entity\Fonction;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -20,7 +21,10 @@ class EmployeType extends AbstractType
             ->add('email')
             ->add('role')
             ->add('login')
-            ->add('mdp', PasswordType::class)
+            ->add('generateMdp', ButtonType::class, [
+                'label' => 'Générer un mot de passe',
+            ])
+            ->add('mdp', TextType::class)
             ->add('fonction', EntityType::class, [
                 'class' => Fonction::class,
                 'choice_label' => function(Fonction $function){
