77b9996b7aaac63ff03ec188cfdc1dab97cb550f
saskia78-byte <saskia78@hotmail.fr>
2026-03-18 18:13:18 +0100

    ajout gestion traductions oeuvre, artiste, contenu enrichi et exposition

--- src/Controller/Admin/OeuvreController.php
+++ src/Controller/Admin/OeuvreController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\TraductionOeuvre;
+use App\Form\TraductionOeuvreType;
 use App\Entity\Oeuvre;
 use App\Form\OeuvreType;
+use App\Repository\LangueRepository;
 use App\Repository\OeuvreRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\Str;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -65,6 +69,62 @@ final class OeuvreController extends AbstractController
         return $this->render('admin/oeuvre/edit.html.twig', [
             'oeuvre' => $oeuvre,
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/traductions/ajouter/{langueCode}', name: 'app_admin_oeuvre_traduction_new', methods: ['GET', 'POST'])]
+    public function ajouterTraduction(Request $request,Oeuvre $oeuvre,string $langueCode,LangueRepository $langueRepository,EntityManagerInterface $entityManager): Response 
+    {
+        $langue = $langueRepository->findOneBy(['code' => $langueCode]);
+        
+        $traduction = new TraductionOeuvre();
+        $traduction->setOeuvre($oeuvre);
+        $traduction->setLangue($langue);
+
+        $form = $this->createForm(TraductionOeuvreType::class, $traduction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($traduction);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_admin_oeuvre_traductions', ['id' => $oeuvre->getId()]);
+        }
+
+        return $this->render('admin/oeuvre/traduction_form.html.twig', [
+            'oeuvre' => $oeuvre,
+            'langue' => $langue,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/traductions/modifier/{tradId}', name: 'app_admin_oeuvre_traduction_edit', methods: ['GET', 'POST'])]
+    public function modifierTraduction(Request $request,Oeuvre $oeuvre,int $tradId,EntityManagerInterface $entityManager): Response 
+    {
+        $traduction = $entityManager->getRepository(TraductionOeuvre::class)->find($tradId);
+
+        $form = $this->createForm(TraductionOeuvreType::class, $traduction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute('app_admin_oeuvre_traductions', ['id' => $oeuvre->getId()]);
+        }
+
+        return $this->render('admin/oeuvre/traduction_form.html.twig', [
+            'oeuvre' => $oeuvre,
+            'langue' => $traduction->getLangue(),
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/traductions', name: 'app_admin_oeuvre_traductions', methods: ['GET'])]
+    public function traductions(Oeuvre $oeuvre,LangueRepository $langueRepository): Response 
+    {
+        $langues = $langueRepository->findAll();
+
+        return $this->render('admin/oeuvre/traductions.html.twig', [
+            'oeuvre' => $oeuvre,
+            'langues' => $langues,
         ]);
     }
 
---
+++ src/Form/TraductionArtisteType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employe;
+use App\Entity\Langue;
+use App\Entity\TraductionArtiste;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TraductionArtisteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('traductionTexte', TextareaType::class, [
+                'required' => false,
+                'attr' => ['rows' => 10],
+            ])
+            ->add('langue', EntityType::class, [
+                'class' => Langue::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('employe', EntityType::class, [
+                'class' => Employe::class,
+                'choice_label' => function(Employe $employe) {
+                    return $employe->getPrenom().' '.$employe->getNom();
+                },
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => TraductionArtiste::class,
+        ]);
+    }
+}
