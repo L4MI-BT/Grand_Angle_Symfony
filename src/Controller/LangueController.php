f863da2999513a91239263649908b0260bad8861
Lucas BT <l.blanchard-toulet@outlook.fr>
2026-03-23 09:39:02 +0100

    correction mineure

--- src/Controller/LangueController.php
+++ src/Controller/LangueController.php
@@ -4,13 +4,12 @@ namespace App\Controller;
 
 use App\Repository\LangueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class LangueController extends AbstractController
 {
 
-    // Method qui permet de changer les valeurs des sessions de langue. 
+    // Method qui permet de changer les valeurs des sessions de langue.
     // On recupère l'Id de la langue dans l'url via _GET (passer par le button dropdown), puis on vérifie si la langue est dans la bdd, puis on récupère les valeurs de l'idLangue, le code, et l'img puis on met à jour les sessions avec ces valeurs.
     
     
